867aaae2d6152a34965d5f3e8b5712c45edb643a
Sleepy105 <lm.sousa@fe.up.pt>
2020-05-15 14:18:11 +0100

    Configure token endpoints

--- auth_server/auth_server/settings.py
+++ auth_server/auth_server/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'restframework_simplejwt',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
--- resource_server/resource_server/settings.py
+++ resource_server/resource_server/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'restframework_simplejwt',
+    'rest_framework_simplejwt',
 ]
 
 MIDDLEWARE = [
