9029e3d8af434775555e539845aa23946ae35914
Tiago Ribeiro <tiago7b27@gmail.com>
2020-05-15 15:24:25 +0000

    Added request verifications

--- auth_server/auth_server/models.py
+++ auth_server/auth_server/models.py
@@ -6,3 +6,13 @@ class User(models.Model):
 
     def __str__(self):
         return self.username
+
+class AuthorizationCode(models.Model):
+    code = models.CharField(max_length=50)
+    client_id = models.CharField(max_length=50)
+    scope = models.CharField(max_length=50)
+    redirect_uri = models.CharField(max_length=100)
+    expiration =  models.DateTimeField()
+
+    def __str__(self):
+        return self.code
--- auth_server/auth_server/views.py
+++ auth_server/auth_server/views.py
@@ -1,32 +1,59 @@
+import random
+import string
+import datetime
+
+from collections import Counter
 from django.http import HttpResponse, HttpResponseRedirect
 from rest_framework.views import APIView
 from django.shortcuts import render, redirect
 
 from auth_server.services.login_service import LoginService
+from auth_server.models import AuthorizationCode
 
 
 class AuthorizationView(APIView):
+
+    scopes = ["read","write","delete"]
+
     def get(self, request):
         error = ""
 
         if not AuthorizationView.validate_authorization_request(request):
-            error = "invalid_request"
+            return redirect("{}?error={}".format(request.GET['redirect_uri'], "invalid_request"))
+
+        received_scopes = request.GET['scope'].split(" ")
+
+        if not all(elem in AuthorizationView.scopes  for elem in received_scopes):
+            return redirect("{}?error={}".format(request.GET['redirect_uri'], "invalid_scope"))
+
+        if request.GET['response_type'] != "code":
+            return redirect("{}?error={}".format(request.GET['redirect_uri'], "unsupported_response_type"))
 
         return render(request, 'authorize.html', {'client_id': request.GET['client_id'],
                                                   'redirect_uri': request.GET['redirect_uri'],
                                                   'scope': request.GET['scope'],
-                                                  'scope_list': request.GET['scope'].split(" "),
+                                                  'scope_list': received_scopes,
                                                   'state': request.GET['state']})
 
 
     def post(self, request):
         if LoginService.check_credentials(request.POST['username'], request.POST['password']):
-            return redirect("{}?code={}&state={}".format(request.GET['redirect_uri'], "oijeofierjgoreijer", request.GET['state']))
+            code = AuthorizationView.generate_authorization_code(request.GET['client_id'], request.GET['redirect_uri'], request.GET['scope'])
+            return redirect("{}?code={}&state={}".format(request.GET['redirect_uri'], code, request.GET['state']))
         else:
             return redirect("{}?error={}".format(request.GET['redirect_uri'], "access_denied"))
 
 
+    @staticmethod
+    def generate_authorization_code(client_id, redirect_uri, scope):
+        lettersAndDigits = string.ascii_letters + string.digits
+        code = ''.join((random.choice(lettersAndDigits) for i in range(15)))
+        now = datetime.datetime.now()
+        expiration = now + datetime.timedelta(minutes=10)
+        new_code = AuthorizationCode(code=code, client_id=client_id, scope=scope, redirect_uri=redirect_uri,expiration=expiration)
+        new_code.save()
 
+        return new_code.code
 
 
     @staticmethod
